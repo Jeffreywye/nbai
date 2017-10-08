be3088ff8d69a5132ee80f63501ff328dc33b183
DJ KHALED WE GLOBAL🔑 <jcgoodru@buffalo.edu>
2017-10-08 16:40:00 -0400

    Updated database schema

--- tables/_base.py
+++ tables/_base.py
@@ -1,43 +1,38 @@
-from mongokit import Document, Connection
+from mongokit import Document
+from connection import connection, DATABASE_NAME
+from fields import Fields as f
+from fields import Structure as s
 import datetime
 
-
-DATABASE_NAME = 'NBAI'
-connection = Connection()
 
 
 def current_utctime():
     return unicode(datetime.datetime.utcnow())
 
-
 @connection.register
-class BaseObject(Document):
+class DatabaseRecord(Document):
 
     __database__ = DATABASE_NAME
 
-    class e:
-        date_created = 'date_created'
-        date_updated = 'date_updated'
-
     structure = {
-        e.date_created : unicode,
-        e.date_updated : unicode,
+        f.date_created : s.date_created,
+        f.date_updated : s.date_updated,
     }
 
     required_fields = [
-        e.date_created,
-        e.date_updated,
+        f.date_created,
+        f.date_updated,
     ]
 
     default_values = {
-        e.date_created : current_utctime,
-        e.date_updated : current_utctime,
+        f.date_created : current_utctime,
+        f.date_updated : current_utctime,
     }
 
     ## Updates the 'date_updated' timestamp
     def save(self):
-        self[self.e.date_updated] = current_utctime()
-        super(BaseObject, self).save()
+        self[f.date_updated] = current_utctime()
+        super(DatabaseRecord, self).save()
         return
 
     ## Allows you to get fields directly,
@@ -50,7 +45,8 @@ class BaseObject(Document):
     ## Allows you to set fields directly
     def __setattr__(self, field, value):
         if field in self.keys():
-            if isinstance(value, basestring):
-                value = unicode(value)
+            #if isinstance(value, basestring):
+            #    value = unicode(value)
             self[field] = value
         Document.__setattr__(self, field, value)
+
