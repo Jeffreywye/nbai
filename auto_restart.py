118e90a5b6d7770658f3ff7920d375ac6fdf62e1
mattalexhoward <mattalexhoward@users.noreply.github.com>
2017-11-25 13:56:25 -0500

    added auto restart and git pull script (#178)
    * added auto restart and git pull script
    
    * changed permissions of script file
    
    * changed the git pull to use a 20s delay to reduce overhead
    
    * changed the time updating logic
    
    * changed hour to be greater than 3 instead of equal to
    
    * removed unnecessary line

---
+++ auto_restart.py
@@ -0,0 +1,25 @@
+#!env/bin/python
+import datetime
+import os
+import time
+
+date_updated = datetime.date.today()
+
+def do_server_update():
+    os.popen("killall server.py").read()
+    os.popen('./src/server.py').read()
+
+def do_git_pull():
+    os.chdir("/var/www/nbai_live")
+    result = os.popen("git pull").read()
+
+    if result != 'Already up-to-date.\n':
+        do_server_update()
+        
+while True:
+    if datetime.date.today() > date_updated and datetime.datetime.now().hour > 3:
+        do_server_update()
+        date_updated = datetime.date.today()
+
+    do_git_pull()
+    time.sleep(20)
